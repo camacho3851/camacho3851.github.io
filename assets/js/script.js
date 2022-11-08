24779d575c0c956790f497c105c0eef36c53ee6f
unknown <mateus@tminformatica.com.br>
2022-11-08 12:04:42 -0300

    Nova versão

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,20 @@
-var i = 0;
-var tag = document.getElementById("text");
-var html = document.getElementById("text").innerHTML;
-var attr = tag.setAttribute("data", html);
-var txt = tag.getAttribute("data");
-var speed = 170;
+window.sr = ScrollReveal();
+ScrollReveal({ distance: '50px' });
+sr.reveal('.vem-esquerda', { duration: 3000, origin: 'left', reset: false })
+sr.reveal('.vem-direita', { duration: 3000, origin: 'right', reset: false })
+sr.reveal('.vem-cima', { duration: 3000, origin: 'top', reset: false })
+sr.reveal('.vem-baixo', { duration: 3000, origin: 'bottom', reset: false })
 
-
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("text").innerHTML = txt.slice(0, i + 1);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-    // console.log(document.getElementById("text").innerHTML);
-}
-
-setTimeout(() => {
-    document.getElementById("text").innerHTML = ''
-    typeWriter();
-
-}, 300)
-
-
-window.sr = ScrollReveal({ reset: true });
-ScrollReveal({ distance: '30px' });
-sr.reveal('.slogan', { duration: 3000, origin: 'left' })
-sr.reveal('.section-titulo', { duration: 3000, origin: 'bottom' })
-sr.reveal('.vem-esquerda', { duration: 3000, origin: 'left' })
-sr.reveal('.vem-direita', { duration: 3000, origin: 'right' })
-sr.reveal('.vem-baixo', { duration: 3000, origin: 'bottom' })
-
-
-
-/* Code for changing active 
-    link on clicking */
-// var btns =
-//     $(".navigation li");
-
-// for (var i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click",
-//         function () {
-//             var current = document
-//                 .getElementsByClassName("active");
-
-//             current[0].className = current[0]
-//                 .className.replace(" active", "");
-
-//             this.className += " active";
-//         });
-// }
-
-// /* Code for changing active 
-// link on Scrolling */
-// $(window).scroll(function () {
-//     var distance = $(window).scrollTop();
-//     $('.page-section').each(function (i) {
-//         // console.log(distance)
-//         if ($(this).position().top
-//             <= distance + 600) {
-
-//             $('.navigation li.active')
-//                 .removeClass('active');
-
-//             $('.navigation li').eq(i)
-//                 .addClass('active');
-//         }
-//     });
-// }).scroll();
-
+sr.reveal('.vem-esquerda-noreset', { duration: 3000, origin: 'left', reset: false })
+sr.reveal('.vem-direita-noreset', { duration: 3000, origin: 'right', reset: false })
+sr.reveal('.vem-cima-noreset', { duration: 3000, origin: 'top', reset: false })
+sr.reveal('.vem-baixo-noreset', { duration: 3000, origin: 'bottom', reset: false })
 
 function resolucao() {
     larguraTela = screen.width
 
     if (larguraTela == 1920) {
-        document.body.style.fontSize = '14px'
+        document.body.style.fontSize = '18px'
     } else if (larguraTela == 1680) {
         document.body.style.fontSize = '12px'
     } else if (larguraTela == 1600) {
@@ -93,10 +34,66 @@ function resolucao() {
     } else if (larguraTela == 1024) {
         document.body.style.fontSize = '9px'
     } else if (larguraTela <= 992) {
-        document.body.style.fontSize = '14px'
+        document.body.style.fontSize = '9px'
     } else {
         document.body.style.fontSize = '14px'
     }
 }
 
 resolucao()
+
+function menu() {
+    var ww = window.innerWidth;
+
+    let checkbox = document.querySelector('.checkbox');
+
+    if (checkbox.checked && ww >= 992) {
+        checkbox.checked = false
+        console.log('tirou o check')
+    }
+}
+
+window.addEventListener('resize', function () {
+    menu()
+    resolucao()
+});
+
+
+
+const links = document.querySelectorAll(".menu-items ul li");
+const hamburger = document.querySelector(".checkbox");
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+
+        setTimeout(() => {
+            hamburger.checked = false;
+        }, 300)
+    })
+})
+
+
+
+function modalimg(img) {
+    var modal = document.getElementById('janelaModal');
+    var modalImg = document.getElementById("imgModal");
+    var captionTexto = document.getElementById("txtImg");
+    var btFechar = document.getElementsByClassName("fechar")[0];
+    var navbar = document.getElementsByClassName("custom-navbar")[0];
+
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+    captionTexto.innerHTML = img.alt;
+    navbar.style.display = "none"
+    
+    btFechar.onclick = function () {
+        navbar.style.display = "block"
+        modal.style.display = "none";
+    }
+    
+    modal.onclick = function () {
+        modal.style.display = "none";
+        navbar.style.display = "block"
+    }
+}
