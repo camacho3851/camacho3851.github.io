9af6bff71d7758ad8e48a18305955db4b8d4ad2b
unknown <mateus@tminformatica.com.br>
2022-11-11 09:45:55 -0300

    Ajuste fonte

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,19 +20,19 @@ function resolucao() {
     } else if (larguraTela == 1600) {
         document.body.style.fontSize = '12px'
     } else if (larguraTela == 1440) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1400) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1366) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1360) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1280) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1152) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela == 1024) {
-        document.body.style.fontSize = '12px'
+        document.body.style.fontSize = '14px'
     } else if (larguraTela <= 992) {
         document.body.style.fontSize = '9px'
     } else {
